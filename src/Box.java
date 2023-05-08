73ad097a36a3e2e8ec0073d7604033afe3aab937
lewis.loughlin <lewispower061@gmail.com>
2023-05-08 15:11:02 +0200

    ö.

--- src/Box.java
+++ src/Box.java
@@ -1,10 +1,34 @@
 import GLOOP.*;
 public class Box{
-    private GLQuader quader; 
-    private Spielfeld feld;
-    private double vX,vZ;
-    private double breite, tiefe;
+    GLQuader quader;
+    double pX,pZ;
+    double breite, tiefe;
 
     public Box() {
+        quader = new GLQuader(0,0,0,100,50,200);
+        quader.setzeFarbe(1,0,0);
+        quader.setzeTextur("src/img/weepingbell.png");
+    }
+    public void bewegeLinks(){
+        quader.verschiebe(-1.5,0,0);
+    }
+    public void bewegeRechts(){
+        quader.verschiebe(1.5,0,0);
+    }
+    public void bewegevorne(){
+        quader.verschiebe(0,0,-1.5);
+    }
+    public void bewegehinten(){
+        quader.verschiebe(0,0,1.5);
+    }
+
+    public double gibX(){
+        return quader.gibX();
+    }
+    public double gibY(){
+        return quader.gibY();
+    }
+    public double gibZ(){
+        return quader.gibZ();
     }
 }
--- src/Kugelfangen.java
+++ src/Kugelfangen.java
@@ -22,7 +22,7 @@ public class Kugelfangen {
     }
 
     public void fuehreAus() {
-        //kys
+
     }
 }
 
